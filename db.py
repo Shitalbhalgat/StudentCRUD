1d38cf8997aee0400ff8c35fd4f33843126874d6
Shitalbhalgat <shitalbhalgat@gmail.com>
2026-08-05 23:46:01 +0530

    db

--- db.py
+++ db.py
@@ -1,16 +1,11 @@
 import os
 import mysql.connector
 
-print("MYSQLHOST =", os.getenv("MYSQLHOST"))
-print("MYSQLPORT =", os.getenv("MYSQLPORT"))
-print("MYSQLUSER =", os.getenv("MYSQLUSER"))
-print("MYSQLDATABASE =", os.getenv("MYSQLDATABASE"))
-
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT", 3306))
+        database=os.getenv("MYSQL_DATABASE"),   # <-- changed
+        port=int(os.getenv("MYSQLPORT"))
     )
