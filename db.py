757ec07a5869b3fa147df37e56bdd783ee6d583f
Shitalbhalgat <shitalbhalgat@gmail.com>
2026-08-06 19:35:50 +0530

    Refactor getconnection to get_connection and format

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ print("PORT:", os.getenv("MYSQLPORT"))
 print("USER:", os.getenv("MYSQLUSER"))
 print("DB:", os.getenv("MYSQLDATABASE"))
 
-def getconnection():
+def get_connection():
     return mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
